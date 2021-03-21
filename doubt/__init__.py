772dce03e69a6b3fdbca78dcc259a6ce9178e84a
Dan Saattrup Nielsen <dansat@erst.dk>
2021-03-21 22:09:27 +0000

    feat: Remove datasets from root __init__

--- doubt/__init__.py
+++ doubt/__init__.py
@@ -3,5 +3,3 @@ from .models import QuantileRegressionForest
 from .models import QuantileLinearRegression
 from .models import QuantileNeuralNetwork
 from .models import Boot
-
-from .datasets import datasets
