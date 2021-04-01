cd53380d5439afdd94843d2a236e1fbb7f4226fe
saattrupdan <saattrupdan@gmail.com>
2021-04-01 13:10:27 +0100

    feat: Remove neural networks

--- doubt/models/__init__.py
+++ doubt/models/__init__.py
@@ -2,4 +2,3 @@ from .boot import Boot # noqa
 from .linear import QuantileLinearRegression # noqa
 from .tree import QuantileRegressionTree # noqa
 from .tree import QuantileRegressionForest # noqa
-from .nn import QuantileNeuralNetwork # noqa
--- doubt/models/nn/__init__.py
+++
@@ -1,1 +0,0 @@
-from .nn import QuantileNeuralNetwork # noqa
