75a81320578bc950991a05b110ba2422234a583e
saattrupdan <saattrupdan@gmail.com>
2021-04-01 15:16:29 +0100

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 
 setup(name='doubt',
-      maintainer='Dan Saattrup Nielsen',
-      maintainer_email='saattrupdan@gmail.com',
-      packages=find_packages(),
-      include_package_data=True,
+      version='1.0.0',
       description='Bringing back uncertainty to machine learning',
-      license='new BSD',
+      long_description=Path('README.md').read_text(),
+      long_description_content_type='text/markdown',
       url='https://github.com/saattrupdan/doubt',
-      version='beta',
-      zip_safe=False,
-      install_requires=['numpy',
-                        'scipy',
-                        'scikit-learn',
-                        'requests',
-                        'PyYAML',
-                        'tables',
-                        'openpyxl'])
+      author='Dan Saattrup Nielsen',
+      author_email='saattrupdan@gmail.com',
+      license='MIT',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8'],
+      packages=find_packages(exclude=('tests',)),
+      include_package_data=True,
+      install_requires=['numpy', 'scipy', 'scikit-learn', 'requests',
+                        'PyYAML', 'tables', 'openpyxl'])
