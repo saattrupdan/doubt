f15c10dc9a4d50388644adfb877df8ef7c5aaa0e
saattrupdan <saattrupdan@gmail.com>
2021-04-11 18:36:19 +0200

    feat: Add `score` method to QuantileLinearRegression

--- doubt/models/linear/qreg.py
+++ doubt/models/linear/qreg.py
@@ -104,3 +104,80 @@ class QuantileLinearRegression(BaseModel):
         if onedim:
             preds = preds.item()
         return preds, intervals
+
+    @staticmethod
+    def _pinball_loss(target: FloatArray,
+                      prediction: FloatArray,
+                      quantile: float) -> float:
+        '''Implementation of the pinball loss.
+
+        This is computed as quantile(target - prediction) if
+        target >= prediction, and (1 - quantile)(prediction - target)
+        otherwise.
+
+        Args:
+            target (float array):
+                The target array, of shape (n,).
+            prediction (float array):
+                The predictions, of shape (n,).
+            quantile (float):
+                The quantile. Needs to be between 0.0 and 1.0.
+
+        Returns:
+            float: The negative pinball loss.
+        '''
+        # Convert inputs to NumPy arrays
+        target_arr = np.asarray(target)
+        prediction_arr = np.asarray(prediction)
+
+        # Compute the residuals
+        res = target_arr - prediction_arr
+
+        # Compute the pinball loss
+        loss = np.mean(np.maximum(res, np.zeros_like(res)) * quantile + \
+                       np.maximum(-res, np.zeros_like(res)) * (1 - quantile))
+        return float(loss)
+
+    def score(self, X: FloatArray, y: FloatArray) -> float:
+        '''Compute either the R^2 value or the negative pinball loss.
+
+        If `uncertainty` is not set in the constructor then the R^2 value will
+        be returned, and otherwise the mean of the two negative pinball losses
+        corresponding to the two quantiles will be returned.
+
+        The pinball loss is computed as quantile * (target - prediction) if
+        target >= prediction, and (1 - quantile)(prediction - target)
+        otherwise.
+
+        Args:
+            X (float array):
+                The array containing the data set, either of shape (n,) or
+                (n, f), with n being the number of samples and f being the
+                number of features.
+            y (float array):
+                The target array, of shape (n,).
+
+        Returns:
+            float: The negative pinball loss.
+        '''
+        # If `uncertainty` has not been specified then simply use the `score`
+        # method from the underlying LinearRegression model, which returns the
+        # R^2 value
+        if self.uncertainty is None:
+            return self._linreg.score(X, y)
+
+        # If `uncertainty` has been specified then compute pinball loss
+        else:
+            # Convert inputs to NumPy arrays
+            X_arr = np.asarray(X)
+            y_arr = np.asarray(y)
+
+            # Define the lower and upper quantiles
+            lower_q = self.uncertainty / 2.
+            upper_q = 1. - lower_q
+
+            # Get the predictions
+            _, intervals = self.predict(X_arr)
+            lower_loss = self._pinball_loss(y_arr, intervals[:, 0], lower_q)
+            upper_loss = self._pinball_loss(y_arr, intervals[:, 1], upper_q)
+            return -(lower_loss + upper_loss) / 2
