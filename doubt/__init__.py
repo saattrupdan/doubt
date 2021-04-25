b96710e94ddbb95f1e696bb605b2651bef2d0c5b
saattrupdan <saattrupdan@gmail.com>
2021-04-25 15:17:14 +0200

    fix: Remove QuantileLinearRegression import

--- doubt/__init__.py
+++ doubt/__init__.py
@@ -2,6 +2,5 @@ __version__ = '2.3.0'  # noqa
 
 from .models import QuantileRegressionTree  # noqa
 from .models import QuantileRegressionForest  # noqa
-from .models import QuantileLinearRegression  # noqa
 from .models import QuantileRegressor  # noqa
 from .models import Boot  # noqa
--- doubt/models/__init__.py
+++ doubt/models/__init__.py
@@ -1,5 +1,4 @@
 from .boot import Boot  # noqa
-from .glm import QuantileLinearRegression  # noqa
 from .glm import QuantileRegressor  # noqa
 from .tree import QuantileRegressionTree  # noqa
 from .tree import QuantileRegressionForest  # noqa
