dfab4885a8f890d62ba28c60c900fd2a94301654
saattrupdan <saattrupdan@gmail.com>
2021-04-25 15:15:40 +0200

    feat: Remove QuantileLinearRegression

--- doubt/models/glm/__init__.py
+++ doubt/models/glm/__init__.py
@@ -1,2 +1,1 @@
-from .linear import QuantileLinearRegression  # noqa
 from .quantile_regressor import QuantileRegressor  # noqa
--- doubt/models/glm/linear.py
+++
@@ -1,7 +0,0 @@
-'''Quantile linear regression'''
-
-from .quantile_regressor import QuantileRegressor
-from sklearn.linear_model import LinearRegression
-
-
-QuantileLinearRegression = QuantileRegressor(LinearRegression())
