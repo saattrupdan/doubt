d444332e9e418d2d4c5a89c754100eef923e4f08
saattrupdan <saattrupdan@gmail.com>
2021-04-25 15:02:57 +0200

    feat: Setup smooth quantile loss optimisation in QuantileRegressor

--- doubt/models/glm/quantile_regressor.py
+++ doubt/models/glm/quantile_regressor.py
@@ -1,10 +1,12 @@
 '''Quantile regression for generalised linear models'''
 
 from .._model import BaseModel
+from .quantile_loss import smooth_quantile_loss
 from .quantile_loss import quantile_loss
 
 from sklearn.linear_model._glm import GeneralizedLinearRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 from typing import Sequence, Tuple, Union, Optional
 from scipy.optimize import minimize
@@ -17,13 +19,22 @@ FloatArray = Sequence[float]
 class QuantileRegressor(BaseModel):
     '''Quantile regression for generalised linear models.
 
+    This uses BFGS optimisation of the smooth quantile loss from [1].
+
     Args:
-        uncertainty (float):
-            The uncertainty in the prediction intervals. Must be between 0
-            and 1. Defaults to 0.05.
         max_iter (int):
             The maximal number of iterations to train the model for. Defaults
             to 10,000.
+        uncertainty (float):
+            The uncertainty in the prediction intervals. Must be between 0
+            and 1. Defaults to 0.05.
+        quantiles (sequence of floats or None, optional):
+            List of quantiles to output, as an alternative to the
+            `uncertainty` argument, and will not be used if that argument
+            is set. If None then `uncertainty` is used. Defaults to None.
+        alpha (float, optional):
+            Smoothing parameter. Defaults to 0.4.
+
 
     Examples:
         Fitting and predicting follows scikit-learn syntax::
@@ -37,43 +48,60 @@ class QuantileRegressor(BaseModel):
             (1030,)
             >>> x = [500, 0, 0, 100, 2, 1000, 500, 20]
             >>> pred, interval = model.predict(x)
-            >>> pred, interval[0], interval[1]
-            (52.672378992388026, 30.418533804253457, 106.94238881241851)
+            >>> pred, interval
+            (78.50224243713615, array([ 19.27889046, 172.71407256]))
+
+    Sources:
+        [1]: Songfeng Zheng (2011). Gradient Descent Algorithms for
+             Quantile Regression With Smooth Approximation. International
+             Journal of Machine Learning and Cybernetics.
     '''
     def __init__(self,
                  model: Union[LinearRegression, GeneralizedLinearRegressor],
+                 max_iter: Optional[int] = None,
                  uncertainty: float = 0.05,
-                 quantiles: Optional[Sequence[float]] = None):
+                 quantiles: Optional[Sequence[float]] = None,
+                 alpha: float = 0.4):
+
+        self.uncertainty = uncertainty
+        self.alpha = alpha
 
         self._model = model
-        self.max_iter = model.max_iter
-        self.uncertainty = uncertainty
-        self._inverse_link_function = None
+        self._scaler = StandardScaler()
 
+        # Set `max_iter` to be the model's `max_iter` attribute if it exists,
+        # and otherwise default to 10,000
+        if max_iter is None:
+            if hasattr(model, 'max_iter'):
+                self.max_iter = model.max_iter
+            else:
+                self.max_iter = 10_000
+        else:
+            self.max_iter = max_iter
+
+        # Default quantiles to the prediction interval
         if quantiles is None:
             self.quantiles = [self.uncertainty / 2, 1 - (self.uncertainty / 2)]
         else:
             self.quantiles = list(quantiles)
 
-        self.weights = {q: None for q in self.quantiles}
+        # Initialise empty inverse link function and weights
+        self._inverse_link_function = None
+        self._weights = {q: None for q in self.quantiles}
 
-    @staticmethod
-    def _objective_function(beta: FloatArray,
-                            X: FloatMatrix,
-                            y: FloatArray,
+    def _objective_function(self,
+                            beta: np.ndarray,
+                            X: np.ndarray,
+                            y: np.ndarray,
                             quantile: float,
                             inverse_link_function: callable) -> float:
         '''Function used to optimise the quantile loss'''
-
-        # Ensure that inputs are Numpy arrays
-        beta_arr = np.asarray(beta)
-        X_arr = np.asarray(X)
-        y_arr = np.asarray(y)
-
-        predictions = inverse_link_function(X_arr @ beta_arr)
-        return quantile_loss(predictions=predictions,
-                             targets=y_arr,
-                             quantile=quantile)
+        predictions = inverse_link_function(X @ beta)
+        loss = smooth_quantile_loss(predictions=predictions,
+                                    targets=y,
+                                    quantile=quantile,
+                                    alpha=self.alpha)
+        return loss
 
     def fit(self,
             X: FloatMatrix,
@@ -89,15 +117,19 @@ class QuantileRegressor(BaseModel):
             y (float array):
                 The target array, of shape (n,).
         '''
-        # Initialise random number generator
-        rng = np.random.default_rng(random_seed)
-
         # Convert inputs to Numpy arrays
         X_arr = np.asarray(X)
         y_arr = np.asarray(y)
 
+        # Fit the scaler and scale `X`
+        self._scaler.fit(X_arr)
+        X_arr = self._scaler.transform(X_arr)
+
         # Fit the underlying model
         self._model.fit(X_arr, y_arr)
+
+        # Add constant feature
+        X_arr = np.concatenate((X_arr, np.ones((X_arr.shape[0], 1))), axis=1)
 
         # When a Scikit-Learn GLM is fitted then its link function is set to
         # the attribute _link_instance. As the LinearRegression class does not
@@ -106,18 +138,29 @@ class QuantileRegressor(BaseModel):
         try:
             link = self._model._link_instance
             self._inverse_link_function = link.inverse
-            self._inverse_link_gradient = link.inverse_derivative
         except AttributeError:
             self._inverse_link_function = lambda x: x
-            self._inverse_link_gradient = lambda x: 1.
+
+        # Use the underlying GLM's parameters for the initial guess of the
+        # quantile weights
+        model_weights = self._model.coef_
+        model_intercept = self._model.intercept_
+        beta_init = np.concatenate((model_weights, [model_intercept]))
 
         # Fit all quantile estimates
         for q in self.quantiles:
-            beta_init = rng.normal(0, 0.3, size=(X_arr.shape[1],))
+
+            # Initialise random seed, which is used by SciPy
+            if random_seed is not None:
+                np.random.seed(random_seed)
+
             args = (X_arr, y_arr, q, self._inverse_link_function)
-            result = minimize(self._objective_function, beta_init, args=args,
-                              jac=True, options={'maxiter': self.max_iter})
-            self.weights[q] = result.x
+            result = minimize(self._objective_function,
+                              beta_init,
+                              args=args,
+                              method='BFGS',
+                              options={'maxiter': self.max_iter})
+            self._weights[q] = result.x
 
         return self
 
@@ -145,15 +188,21 @@ class QuantileRegressor(BaseModel):
         if onedim:
             X_arr = np.expand_dims(X_arr, 0)
 
-        # Get the prediction for the lower- and upper quantiles
-        quantile_vals = [self._inverse_link_function(X_arr @ self.weights[q])
-                         for q in self.weights.keys()]
-
-        # Concatenate the quantiles to get the intervals
-        quantile_vals = np.stack(quantile_vals, axis=1).squeeze()
+        # Standardise the feature matrix
+        X_arr = self._scaler.transform(X_arr)
 
         # Compute the predictions from the underlying model
         preds = self._model.predict(X_arr)
+
+        # Add constant feature
+        X_arr = np.concatenate((X_arr, np.ones((X_arr.shape[0], 1))), axis=1)
+
+        # Get the prediction for the lower- and upper quantiles
+        quantile_vals = [self._inverse_link_function(X_arr @ self._weights[q])
+                         for q in self._weights.keys()]
+
+        # Concatenate the quantiles to get the intervals
+        quantile_vals = np.stack(quantile_vals, axis=1).squeeze()
 
         # If we started with a one-dimensional input, ensure that the output is
         # also one-dimensional
@@ -161,3 +210,43 @@ class QuantileRegressor(BaseModel):
             preds = preds.item()
 
         return preds, quantile_vals
+
+    def score(self, X: Sequence[float], y: Sequence[float]) -> float:
+        '''Compute either the R^2 value or the negative pinball loss.
+
+        If `uncertainty` is not set in the constructor then the R^2 value will
+        be returned, and otherwise the mean of the two negative pinball losses
+        corresponding to the two quantiles will be returned.
+
+        The pinball loss is computed as quantile * (target - prediction) if
+        target >= prediction, and (1 - quantile)(prediction - target)
+        otherwise.
+
+        Args:
+            X (float array):
+                The array containing the data set, either of shape (n,) or
+                (n, f), with n being the number of samples and f being the
+                number of features.
+            y (float array):
+                The target array, of shape (n,).
+
+        Returns:
+            float: The negative pinball loss.
+        '''
+        # Convert inputs to Numpy arrays
+        X_arr = np.asarray(X)
+        y_arr = np.asarray(y)
+
+        # If `uncertainty` has not been specified then simply use the `score`
+        # method from the underlying LinearRegression model, which returns the
+        # R^2 value
+        if self.uncertainty is None:
+            return self._linreg.score(X_arr, y_arr)
+
+        # If `uncertainty` has been specified then compute pinball loss
+        else:
+            # Get the predictions
+            _, quantile_vals = self.predict(X_arr)
+            losses = [quantile_loss(y_arr, quantile_vals[:, i], q)
+                      for i, q in enumerate(self.quantiles)]
+            return -np.mean(losses)
